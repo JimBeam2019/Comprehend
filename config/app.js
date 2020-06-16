440ae7a704a657905159cf98b0c579d03cf32941
JimBeam2019 <bingking68@hotmail.com>
2020-06-16 11:48:58 +0800

    - Basic setup.

---
+++ config/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+/**
+ *
+ *
+ * @class App
+ */
+class App {
+  /**
+   * Creates an instance of App.
+   * @memberof App
+   */
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  /**
+   *
+   *
+   * @memberof App
+   */
+  config() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+}
+
+export default new App().app;
